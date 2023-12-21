d1f3ea4700a8222eabf28a8bccfdeb3f5f59f7f3
Drew Benson <Drewbenson16@gmail.com>
2023-12-21 11:32:18 -0700

    array filter

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const lowercaseName = name.toLowerCase();
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowercaseName);
+    return matchingDrivers;
+  }
+  
+  function fuzzyMatch(drivers, query) {
+    const matchingDrivers = drivers.filter(driver => driver.startsWith(query));
+    return matchingDrivers;
+  }
+
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  // Example usage
+  const drivers = [
+    { name: 'Ada', age: 30 },
+    { name: 'Bob', age: 25 },
+    { name: 'Charlie', age: 35 },
+  ];
+  
+  const matchedDrivers = matchName(drivers, 'Ada');
+  console.log(matchedDrivers);
